8a01c648990f5ddea6f3d6e8b0dd34696a463d0a
Isabela Cruz <cruzisabela@hotmail.com>
2025-11-19 09:28:07 -0300

    WIP: Inicio tela Cadastro

---
+++ src/app/tela/landing/landing.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { PageCadastro } from '../../componente/cadastro/cadastro';
+
+@Component({
+  selector: 'landing',
+  imports: [PageCadastro],
+  templateUrl: './landing.html',
+  styleUrl: './landing.css',
+})
+export class PageLanding {
+
+}
