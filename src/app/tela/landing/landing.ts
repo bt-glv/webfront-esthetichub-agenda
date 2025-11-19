184aa512ccd5f28f14a8f1632621c7945d337b98
Aquiles7-ui <aquiles.miqueias2019@gmail.com>
2025-11-19 09:29:14 -0300

    WIP.tela de login

---
+++ src/app/tela/landing/landing.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Login } from '../../componente/login/login';
+
+@Component({
+  selector: 'landing',
+  imports: [Login],
+  templateUrl: './landing.html',
+  styleUrl: './landing.css',
+})
+export class PageLanding {
+
+}
