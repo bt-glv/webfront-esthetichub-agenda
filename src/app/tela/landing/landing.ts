e7bd2d9f213c209fdf95671d24b170aa586cd2eb
bt-glv <bruno@glv.sh>
2025-11-24 10:01:47 -0300

    WIP(landing): Formatar o Banner inicial

--- src/app/tela/landing/landing.ts
+++ src/app/tela/landing/landing.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
 import { NavBar } from '../../componente/nav-bar/nav-bar';
+import { NgOptimizedImage } from '@angular/common';
+
 @Component({
   selector: 'landing',
-  imports: [NavBar],
+  imports: [NavBar, NgOptimizedImage],
   templateUrl: './landing.html',
   styleUrl: './landing.css',
 })
