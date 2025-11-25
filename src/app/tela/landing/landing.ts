4c15302178fc284ed56e749adbb6deced42ab4ef
Isabela Cruz <cruzisabela@hotmail.com>
2025-11-25 17:03:49 -0300

    feat: criação da tela dia e horário

--- src/app/tela/landing/landing.ts
+++ src/app/tela/landing/landing.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { PageCadastro } from '../../componente/cadastro/cadastro';
 import { SelecionarFuncionario } from '../../componente/selecionar-funcionario/selecionar-funcionario';
+import { SelecionarHorario } from '../../componente/selecionar-horario/selecionar-horario';
 
 @Component({
   selector: 'landing',
-  imports: [PageCadastro, SelecionarFuncionario],
+  imports: [PageCadastro, SelecionarFuncionario, SelecionarHorario],
   templateUrl: './landing.html',
   styleUrl: './landing.css',
 })
