fded2dd6cd759d9f2060fea97c194684b933ab59
Aquiles7-ui <aquiles.miqueias2019@gmail.com>
2025-11-25 15:56:09 -0300

    página confirmação de agendamento

--- src/app/tela/landing/landing.ts
+++ src/app/tela/landing/landing.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { PageCadastro } from '../../componente/cadastro/cadastro';
 import { Login } from '../../componente/login/login';
 import { SelecionarServico } from '../../componente/selecionar-servico/selecionar-servico';
+import { SelecionarConfirmacao } from '../../componente/selecionar-confirmacao/selecionar-confirmacao';
 
 @Component({
   selector: 'landing',
-  imports: [PageCadastro, Login, SelecionarServico],
+  imports: [PageCadastro, Login, SelecionarServico, SelecionarConfirmacao],
   templateUrl: './landing.html',
   styleUrl: './landing.css',
 })
