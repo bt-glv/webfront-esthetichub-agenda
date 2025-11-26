08c9b87bf496f6bc739dbd3060449f2c93059e35
Isabela Cruz <cruzisabela@hotmail.com>
2025-11-26 08:17:20 -0300

    adicionar componente de teste

--- src/app/tela/landing/landing.ts
+++ src/app/tela/landing/landing.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { NavBar } from '../../componente/nav-bar/nav-bar';
 import { NgOptimizedImage } from '@angular/common';
+import { PageCadastro } from '../../componente/cadastro/cadastro';
+import { SelecionarFuncionario } from '../../componente/selecionar-funcionario/selecionar-funcionario';
+import { SelecionarHorario } from '../../componente/selecionar-horario/selecionar-horario';
 
 @Component({
   selector: 'landing',
-  imports: [NavBar, NgOptimizedImage],
+  imports: [NavBar, NgOptimizedImage, PageCadastro, SelecionarFuncionario, SelecionarHorario],
   templateUrl: './landing.html',
   styleUrl: './landing.css',
 })
