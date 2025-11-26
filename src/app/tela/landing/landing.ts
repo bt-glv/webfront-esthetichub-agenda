b9dd84c727d91c8353e7f5ec818726cbce841d72
Isabela Cruz <cruzisabela@hotmail.com>
2025-11-26 09:11:21 -0300

    feat: Alteração tela de cadastro

--- src/app/tela/landing/landing.ts
+++ src/app/tela/landing/landing.ts
@@ -4,10 +4,13 @@ import { NgOptimizedImage } from '@angular/common';
 import { PageCadastro } from '../../componente/cadastro/cadastro';
 import { SelecionarFuncionario } from '../../componente/selecionar-funcionario/selecionar-funcionario';
 import { SelecionarHorario } from '../../componente/selecionar-horario/selecionar-horario';
+import { Login } from '../../componente/login/login';
+import { SelecionarConfirmacao } from '../../componente/selecionar-confirmacao/selecionar-confirmacao';
+import { SelecionarServico } from '../../componente/selecionar-servico/selecionar-servico';
 
 @Component({
   selector: 'landing',
-  imports: [NavBar, NgOptimizedImage, PageCadastro, SelecionarFuncionario, SelecionarHorario],
+  imports: [NavBar, NgOptimizedImage, PageCadastro, SelecionarFuncionario, SelecionarHorario, Login, SelecionarConfirmacao,SelecionarServico],
   templateUrl: './landing.html',
   styleUrl: './landing.css',
 })
