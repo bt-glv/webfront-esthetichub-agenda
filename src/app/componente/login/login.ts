5765008dfe4a6b1c1276b5cef3cd0ce194570d40
Joao Victor <joaovictor15308@gmail.com>
2025-11-26 22:48:24 -0300

    feat: Adiciona databiding e cadastro de usuarios

--- src/app/componente/login/login.ts
+++ src/app/componente/login/login.ts
@@ -1,16 +1,22 @@
 import { Component, Output, EventEmitter } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
 export class Login {
     @Output() open_cadastro    = new EventEmitter<void>()
     @Output() open_agendamento = new EventEmitter<void>()
-
+    login: string = ''
+    senha: string = ''
+    
     btnLogin() {
+        console.log('Dados do formulário:');
+        console.log('login:', this.login);
+        console.log('senha:', this.senha);
         this.open_agendamento.emit()
     }
     btnCadastro() {
