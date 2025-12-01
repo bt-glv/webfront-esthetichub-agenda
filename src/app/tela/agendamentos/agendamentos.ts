2e43c0576430104a48358e2bfa2455e71bd98309
bt-glv <bruno@glv.sh>
2025-12-01 08:30:08 -0300

    feat(agendamentos): adicionar componentes para teste

--- src/app/tela/agendamentos/agendamentos.ts
+++ src/app/tela/agendamentos/agendamentos.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { NavBar } from '../../componente/nav-bar/nav-bar';
+import { SelecionarHorario } from '../../componente/selecionar-horario/selecionar-horario';
+import { SelecionarServico } from '../../componente/selecionar-servico/selecionar-servico';
+import { SelecionarFuncionario } from '../../componente/selecionar-funcionario/selecionar-funcionario';
+import { SelecionarConfirmacao } from '../../componente/selecionar-confirmacao/selecionar-confirmacao';
 
 @Component({
   selector: 'app-agendamentos',
-  imports: [NavBar],
+  imports: [NavBar, SelecionarHorario, SelecionarServico, SelecionarFuncionario, SelecionarConfirmacao],
   templateUrl: './agendamentos.html',
   styleUrl: './agendamentos.css',
 })
